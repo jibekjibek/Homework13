f16427363a31affbb11115b8825874005f67d4b9
JibekJibek <tadzhibayevajbk@gmail.com>
2019-04-26 13:06:26 +0600

    first commit

---
+++ com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "tolia";
+        int age = 21;
+        int air = 21;
+
+
+        if (air >30 || air <20) {
+            if (age >= 20 && age <= 45)
+            System.out.println("Don't go out!");
+        }   else { System.out.println("Go friend");
+        }
+        if (air >=0 && air <=28) {
+           if (age >=20) {
+               System.out.println("Don't go out");
+           } else {
+               System.out.println("Go friend");
+           }
+        }
+
+            }
+
+
+        }
+
+
+
+
+
